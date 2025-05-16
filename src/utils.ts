bccda3511115b4976bd2341e7fd187ee98e25235
Jon Goodger <jonno.is@gmail.com>
2025-05-16 15:17:05 +0100

    Add execC function for more control over shell execution

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,12 @@
 import type { PathLike } from 'node:fs'
 import { stat } from 'node:fs/promises'
+import type { BaseContext } from 'clipanion'
+import type { Options } from 'execa'
+import { execa } from 'execa'
+
+type ExecCOptions = Options & {
+  context?: BaseContext
+}
 
 export async function fileExists(path: PathLike): Promise<boolean> {
   try {
@@ -15,4 +22,18 @@ export function constToCamel(key: string): string {
   parts[0] = parts[0].toLocaleLowerCase()
 
   return parts.join('')
+}
+
+export async function execC(command: string, args: string[], options: ExecCOptions = {}) {
+  const allOptions = {
+    shell: true,
+    ...options,
+  }
+
+  if (options.context) {
+    allOptions.stdout ||= options.context.stdout
+    allOptions.stderr ||= options.context.stderr
+  }
+
+  return await execa(command, args, allOptions)
 }
