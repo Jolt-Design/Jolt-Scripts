22bf45abf71a6bdf2766e817cff5e8bae73a659d
Jon Goodger <jonno.is@gmail.com>
2025-05-20 08:57:34 +0100

    Add JoltCommand.getHeader()

--- src/JoltCommand.ts
+++ src/JoltCommand.ts
@@ -1,11 +1,19 @@
+import chalk from 'chalk'
 import { Command } from 'clipanion'
 import type { Config } from './Config.js'
 import getConfig from './Config.js'
 
 export default abstract class JoltCommand extends Command {
+  logo = chalk.magentaBright('⚡')
   config!: Config
+  requiredCommands: string[] = []
 
   abstract command(): Promise<number | undefined>
+
+  getHeader(suffix = '') {
+    const header = chalk.whiteBright(`${this.logo} ${this.cli.binaryLabel}`)
+    return [header, suffix, '\n\n'].filter((x) => !!x).join(' ')
+  }
 
   async execute(): Promise<number | undefined> {
     this.config = await getConfig()
