2e0999d80d0c8cc39b2f6d7d891327b3406731de
Jon Goodger <jonno.is@gmail.com>
2025-09-02 03:40:16 +0100

    Implement utils.keyToConst()

--- src/utils.ts
+++ src/utils.ts
@@ -34,6 +34,16 @@ export function constToCamel(key: string): string {
   return parts.join('')
 }
 
+export function keyToConst(str: string): string {
+  return (
+    str
+      .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+      ?.map((x) => x.toLowerCase())
+      .join('_')
+      .toUpperCase() || str
+  )
+}
+
 export async function execC(
   command: string,
   args: (string | null | undefined | false)[] = [],
